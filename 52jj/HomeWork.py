6818e6f7a2a59e4f6a52d96b4d8320b29ccfd41c
jingjing-123-an <2723283335@qq.com>
2019-12-03 19:21:17 +0800

    Create HomeWork.py
    1234

---
+++ 52jj/HomeWork.py
@@ -0,0 +1,16 @@
+import sys;x="runoob";sys.stdout.write(x+'\n')
+
+str='Runoob'
+
+print(str)
+print(str[0:-1])
+print(str[0])
+print(str[2:5])
+print(str[2:])
+print(str+"你好")
+print('------------------------------')
+print("Hello\nrunnob")
+print(r"Hello\nrunnob")
+input("\n\n按下enter键后退出.\n")
+message=input("Tell me something,and I will repeat it back to you:")
+print(message)
